83625cc73e4e9dd260eb641e3e16323281ccb18f
Ahmed Alwahib <aalwheep@gmail.com>
2023-01-24 19:03:10 +0300

    Init

---
+++ src/main/java/com/alwahib/java_lambdas_streams/App.java
@@ -0,0 +1,13 @@
+package com.alwahib.java_lambdas_streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
