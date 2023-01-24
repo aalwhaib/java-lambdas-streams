54ce603a7bec6d75ffea114c8bcc712a21f21c01
Ahmed Alwahib <aalwheep@gmail.com>
2023-01-24 21:39:26 +0300

    #03_05

---
+++ src/main/java/com/alwahib/java_lambdas_streams/planet/PlanetUtils.java
@@ -0,0 +1,26 @@
+package com.alwahib.java_lambdas_streams.planet;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PlanetUtils {
+
+	public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
+		return planets.stream()
+				.filter(planet -> planet.getName().startsWith("M"))
+				.collect(Collectors.toList());
+	}
+
+	public List<Planet> getPlanetsWithRings(List<Planet> planets) {
+		return null;
+	}
+
+	public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
+		return null;
+	}
+
+	public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
+		return null;
+	}
+
+}
