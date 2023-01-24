eb18246afa7fa11534214b45ad3f7468a7dd7b1e
Ahmed Alwahib <aalwheep@gmail.com>
2023-01-24 20:43:57 +0300

    #02_04b

---
+++ src/main/java/com/alwahib/java_lambdas_streams/emp/Employee.java
@@ -0,0 +1,50 @@
+package com.alwahib.java_lambdas_streams.emp;
+
+public class Employee {
+
+	private String firstName;
+	private String lastName;
+	private int yearsOfService;
+	private Employee manager;
+	private int numberOfDirectReports;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getYearsOfService() {
+		return yearsOfService;
+	}
+
+	public void setYearsOfService(int yearsOfService) {
+		this.yearsOfService = yearsOfService;
+	}
+
+	public Employee getManager() {
+		return manager;
+	}
+
+	public void setManager(Employee manager) {
+		this.manager = manager;
+	}
+
+	public int getNumberOfDirectReports() {
+		return numberOfDirectReports;
+	}
+
+	public void setNumberOfDirectReports(int numberOfDirectReports) {
+		this.numberOfDirectReports = numberOfDirectReports;
+	}
+}
